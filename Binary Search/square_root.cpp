22f731c5f04c45834918ba0ce377e8caad27453f
p1yush <rr2rinku@gmail.com>
2021-12-08 22:11:55 +0530

    binary search

---
+++ Binary Search/square_root.cpp
@@ -0,0 +1,61 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+using namespace std;
+
+
+
+#define ll              long long
+#define pb              push_back
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n)       int *arr=new int[n];
+#define t(x)            int t; cin>>t; while(t--)
+#define fa(n)        for(int i=0; i<n; i++)
+#define fr(n)           for(int j=n-1; j<=0; j--)
+#define f(x,y)          for(int i=x; i<=y; i++)
+#define vi(v, n)        vector<long long> v(n);
+
+
+
+void r_r_2() {
+   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+}
+
+int sqrt(int n) {
+   int s = 0, e = n;
+   int mid = s + (e - s) / 2;
+   int ans = -1;
+   while (s <= e) {
+      if (mid * mid == n)
+         return mid;
+      else if (mid  * mid < n) {
+         ans = mid;
+         s = mid + 1;
+      }
+      else
+         e = mid - 1;
+      mid = s + (e - s) / 2;
+   }
+   return ans;
+}
+
+
+void solve() {
+   int n;
+   cin >> n;
+   cout << sqrt(n);
+}
+
+
+
+int main()
+{
+   r_r_2();
+   solve();
+   return 0;
+}
