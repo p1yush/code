2f8d6f62a94e46c8d81d716dbb0ef74bff200605
p1yush <rr2rinku@gmail.com>
2021-12-19 22:32:26 +0530

    codechef

---
+++ Codechef/MUSICHAIR.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+using namespace std;
+
+
+
+#define ll              long long
+#define pb              push_back
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n)       int *arr=new int[n];
+#define t(x)            int t; cin>>t; while(t--)
+#define fa(n)        for(int i=0; i<n; i++)
+#define fr(n)           for(int j=n-1; j<=0; j--)
+#define f(x,y)          for(int i=x; i<=y; i++)
+#define vi(v, n)        vector<long long> v(n);
+
+
+
+void r_r_2() {
+   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+}
+
+
+
+void solve() {
+   ll n;
+   cin >> n;
+   int count = 0;
+   n -= 1;
+   for (int i = 1; i <= sqrt(n); i++)
+      if (n % i == 0)
+      {
+         if (n / i == i)
+            count++;
+         else
+            count += 2;
+      }
+   cout << count;
+}
+
+
+
+int main()
+{
+   r_r_2();
+   t(x) {
+      solve();
+      cout << endl;
+   }
+   return 0;
+}
