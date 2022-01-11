aa1af3a5a5f8855e56e3b60aa79825bdd7a515af
p1yush <rr2rinku@gmail.com>
2022-01-11 19:33:40 +0530

    recurssion

---
+++ Recursion/fibonacci.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+using namespace std;
+
+
+
+#define ll              long long
+#define pb              push_back
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n)       int *arr=new int[n];
+#define t(x)            int t; cin>>t; while(t--)
+#define fa(n)        for(int i=0; i<n; i++)
+#define fr(n)           for(int j=n-1; j<=0; j--)
+#define f(x,y)          for(int i=x; i<=y; i++)
+#define vi(v, n)        vector<long long> v(n);
+
+void r_r_2() {
+   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+}
+
+int fibonacci(int n) {
+   if (n == 0 || n == 1)
+      return n;
+   return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+void solve() {
+   int n;
+   cin >> n;
+   cout << fibonacci(n);
+}
+
+
+int main()
+{
+   r_r_2();
+   solve();
+   return 0;
+}
