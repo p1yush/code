05ea41daeead0326bc766331bbdb9cb8fb6c526b
p1yush <rr2rinku@gmail.com>
2022-02-28 19:23:35 +0530

    Linked List

---
+++ Linked List/Sort012.cpp
@@ -0,0 +1,121 @@
+#include<bits/stdc++.h>
+#include<stdlib.h>
+using namespace std;
+
+
+
+#define ll              long long
+#define pb              push_back
+#define mod             1000000007
+#define inf             1e18
+#define ps(x,y)         fixed<<setprecision(y)<<x
+#define mk(arr,n)       int *arr=new int[n];
+#define t(x)            int t; cin>>t; while(t--)
+#define fa(n)        for(int i=0; i<n; i++)
+#define fr(n)           for(int j=n-1; j<=0; j--)
+#define f(x,y)          for(int i=x; i<=y; i++)
+#define vi(v, n)        vector<long long> v(n);
+
+void r_r_2() {
+   ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#ifndef ONLINE_JUDGE
+   freopen("input.txt", "r", stdin);
+   freopen("output.txt", "w", stdout);
+#endif
+}
+
+class Node {
+public:
+   int data;
+   Node* next;
+
+   Node(int data) {
+      this -> data = data;
+      this -> next = NULL;
+   }
+};
+
+Node *takeInput() {
+   int data;
+   cin >> data;
+   Node *head = NULL;
+   Node *tail = NULL;
+   while (data != -1) {
+      Node *newNode = new Node(data);
+      if (head == NULL) {
+         head = newNode;
+         tail = newNode;
+      }
+      else {
+         tail -> next = newNode;
+         tail = newNode;
+      }
+      cin >> data;
+   }
+   return head;
+}
+
+Node *LastElement(Node *head) {
+   Node *tail = head;
+   while (tail -> next != NULL) {
+      tail = tail -> next;
+   }
+   return tail;
+}
+
+void print(Node* head) {
+   Node *temp = head;
+   while (temp != NULL) {
+      cout << temp -> data << " ";
+      temp = temp -> next;
+   }
+}
+
+
+Node *sortList(Node *head) {
+   int zeroCount = 0;
+   int oneCount = 0;
+   int twoCount = 0;
+   Node *temp = head;
+   while (temp != NULL) {
+      if (temp -> data == 0)
+         zeroCount++;
+      else if (temp -> data == 1)
+         oneCount++;
+      else if (temp -> data == 2)
+         twoCount++;
+      temp = temp -> next;
+   }
+   temp = head;
+   while (temp != NULL) {
+      if (zeroCount != 0) {
+         temp -> data = 0;
+         zeroCount--;
+      }
+      else if (oneCount != 0) {
+         temp -> data = 1;
+         oneCount--;
+      }
+      else if (twoCount != 0) {
+         temp -> data = 2;
+         twoCount--;
+      }
+      temp = temp -> next;
+   }
+   return head;
+}
+
+
+void solve() {
+   Node *head = takeInput();
+   Node *first = sortList(head);
+   print(first);
+}
+
+
+int main()
+{
+   r_r_2();
+   solve();
+   return 0;
+}
