936dd37ede06f5869d0813facc28cac5b0b6c380
p1yush <rr2rinku@gmail.com>
2022-05-07 19:38:40 +0530

    Recursion

---
+++ Recursion/ReachNStairs.cpp
@@ -0,0 +1,8 @@
+int countDistinctWayToClimbStair(long long nStairs)
+{
+    if(nStairs < 0)
+		return 0;
+	if(nStairs == 0)
+		return 1;
+	return countDistinctWayToClimbStair(nStairs - 1) + countDistinctWayToClimbStair(nStairs - 2);
+}
